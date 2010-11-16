304ea073b912df42a75b38cdaed2f856e4acb24b
reddavis <reddavis@gmail.com>
2010-11-16 12:06:13 +0000

    work with nested hashes and arrays

--- lib/faraday/ext/enumerable.rb
+++ lib/faraday/ext/enumerable.rb
@@ -1,16 +1,18 @@
 module Enumerable
-  def to_param(parts=nil, base=nil)
+  def to_param(parts=nil, previous_base=nil)
     parts = []
 
     self.to_a.each do |key, value|
+      base = build_base(key, previous_base)
+
       case value
       when Array
         values = value.map(&:strip).join(",")
-        parts << build_param(key, values, base)
+        parts << build_param(base, values)
       when Hash
-        parts << value.to_param(parts, key)
+        parts << value.to_param(parts, base)
       else
-        parts << build_param(key, value, base)
+        parts << build_param(base, value)
       end
     end
 
@@ -19,11 +21,15 @@ module Enumerable
 
   private
 
-  def build_param(key, value, base)
-    if base
-      "#{base}[#{key}]=#{value}"
-    else
-      "#{key}=#{value}"
-    end
+  # {:a => {:b => {:c => "d"}}} => a[b][c]...
+  # {:a => {:b => "d"}} => a[b]...
+  def build_base(key, previous_base)
+    return key unless previous_base
+
+    "#{previous_base}[#{key}]"
+  end
+
+  def build_param(base, value)
+    "#{base}=#{value}"
   end
 end
--- spec/faraday/ext/enumerable_spec.rb
+++ spec/faraday/ext/enumerable_spec.rb
@@ -16,6 +16,20 @@ describe "Enumberable ext" do
       it { should eql("a[b]=c") }
     end
 
+    describe "multiple nested" do
+      let(:hash) { @hash = {:a => {:b => {:c => "d"}}} }
+      subject { hash.to_param }
+
+      it { should eql("a[b][c]=d") }
+    end
+
+    describe "multiple params" do
+      let(:hash) { @hash = {:a => "b", :c => "d"} }
+      subject { hash.to_param }
+
+      it { should eql("a=b&c=d") }
+    end
+
     describe "hash containing an array" do
       let(:hash) { @hash = {:a => ["b", "c"]} }
       subject { hash.to_param }
