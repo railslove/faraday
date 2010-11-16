6e29896840ef2c9aa387e357ed3cab6ebaf3a03a
reddavis <reddavis@gmail.com>
2010-11-16 10:25:31 +0000

    move the autoload helper to its own file

--- lib/faraday.rb
+++ lib/faraday.rb
@@ -1,4 +1,8 @@
+require 'faraday/autoload_helper'
+
 module Faraday
+  extend AutoloadHelper
+
   VERSION = "0.5.3"
 
   class << self
@@ -17,38 +21,6 @@ module Faraday
   def self.default_connection
     @default_connection ||= Connection.new
   end
-
-  module AutoloadHelper
-    def register_lookup_modules(mods)
-      (@lookup_module_index ||= {}).update(mods)
-    end
-
-    def lookup_module(key)
-      return if !@lookup_module_index
-      const_get @lookup_module_index[key] || key
-    end
-
-    def autoload_all(prefix, options)
-      options.each do |const_name, path|
-        autoload const_name, File.join(prefix, path)
-      end
-    end
-
-    # Loads each autoloaded constant.  If thread safety is a concern, wrap
-    # this in a Mutex.
-    def load_autoloaded_constants
-      constants.each do |const|
-        const_get(const) if autoload?(const)
-      end
-    end
-
-    def all_loaded_constants
-      constants.map { |c| const_get(c) }.
-        select { |a| a.respond_to?(:loaded?) && a.loaded? }
-    end
-  end
-
-  extend AutoloadHelper
 
   autoload_all 'faraday',
     :Middleware      => 'middleware',
